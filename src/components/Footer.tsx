49b38ce7e340b549fedaaed1b91531228f3da0cb
Julia <juliamachadosouza@gmail.com>
2023-11-22 16:01:30 +0100

    traducao footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,43 +1,33 @@
 import Image from "next/image";
 import Link from "next/link";
 import ThemeSwitchOption from "./ThemeSwitchOption";
+import { useTranslation } from "react-i18next";
 
 const social = [
   {
     name: "facebook",
     href: "https://www.facebook.com/",
     icon: (
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="none">
-        <path
-          d="M504 256C504 119 393 8 256 8S8 119 8 256c0 123.78 90.69 226.38 209.25 245V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.28c-30.8 0-40.41 19.12-40.41 38.73V256h68.78l-11 71.69h-57.78V501C413.31 482.38 504 379.78 504 256z"
-          fill="currentColor"
-        />
-      </svg>
+      <svg width="24px" height="24px" viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg">
+        <path fill="black" d="M14,6h3a1,1,0,0,0,1-1V3a1,1,0,0,0-1-1H14A5,5,0,0,0,9,7v3H7a1,1,0,0,0-1,1v2a1,1,0,0,0,1,1H9v7a1,1,0,0,0,1,1h2a1,1,0,0,0,1-1V14h2.22a1,1,0,0,0,1-.76l.5-2a1,1,0,0,0-1-1.24H13V7A1,1,0,0,1,14,6Z"/>
+        </svg>
     ),
   },
   {
     name: "instagram",
     href: "https://www.instagram.com/",
     icon: (
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" fill="none">
-        <path
-          d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"
-          fill="currentColor"
-        />
-      </svg>
+      <svg fill="#000000" width="29px" height="29px" viewBox="0 0 23 24" xmlns="http://www.w3.org/2000/svg">
+      <path fill-rule="evenodd" d="M8,2 L16,2 C19.3137085,2 22,4.6862915 22,8 L22,16 C22,19.3137085 19.3137085,22 16,22 L8,22 C4.6862915,22 2,19.3137085 2,16 L2,8 C2,4.6862915 4.6862915,2 8,2 Z M8,4 C5.790861,4 4,5.790861 4,8 L4,16 C4,18.209139 5.790861,20 8,20 L16,20 C18.209139,20 20,18.209139 20,16 L20,8 C20,5.790861 18.209139,4 16,4 L8,4 Z M12,17 C9.23857625,17 7,14.7614237 7,12 C7,9.23857625 9.23857625,7 12,7 C14.7614237,7 17,9.23857625 17,12 C17,14.7614237 14.7614237,17 12,17 Z M12,15 C13.6568542,15 15,13.6568542 15,12 C15,10.3431458 13.6568542,9 12,9 C10.3431458,9 9,10.3431458 9,12 C9,13.6568542 10.3431458,15 12,15 Z M17,8 C16.4477153,8 16,7.55228475 16,7 C16,6.44771525 16.4477153,6 17,6 C17.5522847,6 18,6.44771525 18,7 C18,7.55228475 17.5522847,8 17,8 Z"/>
+    </svg>
     ),
   },
 
   {
-    name: "youtube",
+    name: "email",
     href: "https://www.youtube.com/",
     icon: (
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" fill="none">
-        <path
-          d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z"
-          fill="currentColor"
-        />
-      </svg>
+      <svg width="24px" height="24px" viewBox="0 0 23 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2 6a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6zm3.519 0L12 11.671 18.481 6H5.52zM20 7.329l-7.341 6.424a1 1 0 0 1-1.318 0L4 7.329V18h16V7.329z" fill="#0D0D0D"/></svg>
     ),
   },
 
@@ -45,22 +35,9 @@ const social = [
     name: "linkedin",
     href: "https://www.linkedin.com/",
     icon: (
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" fill="none">
-        <path
-          d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"
-          fill="currentColor"
-        />
-      </svg>
+      <svg fill="#000000" width="24px" height="24px" viewBox="-2 -3 24 23" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin" ><path d='M19.959 11.719v7.379h-4.278v-6.885c0-1.73-.619-2.91-2.167-2.91-1.182 0-1.886.796-2.195 1.565-.113.275-.142.658-.142 1.043v7.187h-4.28s.058-11.66 0-12.869h4.28v1.824l-.028.042h.028v-.042c.568-.875 1.583-2.126 3.856-2.126 2.815 0 4.926 1.84 4.926 5.792zM2.421.026C.958.026 0 .986 0 2.249c0 1.235.93 2.224 2.365 2.224h.028c1.493 0 2.42-.989 2.42-2.224C4.787.986 3.887.026 2.422.026zM.254 19.098h4.278V6.229H.254v12.869z' /></svg>
     ),
   },
-];
-
-const column1 = [
-  { name: "Home", href: "/" },
-  { name: "Sobre", href: "/about" },
-  { name: "Serviços", href: "/services" },
-  { name: "Portfólio", href: "/portfolio" },
-  { name: "Contato", href: "/contact" },
 ];
 
 const column2 = [
@@ -77,6 +54,14 @@ const column3 = [
 ];
 
 export default function Footer() {
+  const { t, i18n } = useTranslation();
+  const column1 = [
+    { name: t('navbar.home'), href: "/" },
+    { name: t('navbar.about'), href: "/about" },
+    { name: t('navbar.services'), href: "/services" },
+    { name: t('navbar.portfolio'), href: "/portfolio" },
+    { name: t('navbar.contact'), href: "/contact" },
+  ];
   return (
     <footer className="bg-neutral-900 text-neutral-300 dark:text-neutral-400">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pt-12 pb-12 grid sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-6">
@@ -94,8 +79,7 @@ export default function Footer() {
           </Link>
 
           <p className="md:text-sm pt-4 w-auto sm:w-6/12 md:w-6/12 lg:w-8/12">
-            Halley is ipsum dolor sit amet consectetur adipisicing itaque enim
-            labore dolores culpa consequatur harum.
+          {t('footer.description')}
           </p>
 
           <div className="flex gap-2 pt-6 sm:pt-6 md:pt-6 lg:pt-12 max-w-[300px]">
@@ -109,9 +93,9 @@ export default function Footer() {
           </div>
         </div>
 
-        <div className=" lg:col-start-5">
+        <div className=" lg:col-start-6">
           <h3 className="text-sm md:text-xs font-bold pb-4 pt-12 lg:pt-0">
-            Páginas
+          {t('footer.pages')}
           </h3>
           {column1.map((item) => (
             <Link
@@ -124,20 +108,7 @@ export default function Footer() {
           ))}
         </div>
 
-        <div >
-          <h3 className="text-sm md:text-xs font-bold pb-4 pt-12 lg:pt-0">
-            Serviços
-          </h3>
-          {column2.map((item) => (
-            <Link
-              key={item.name}
-              href={item.href}
-              className="hover:text-orange md:text-sm my-4 grid"
-            >
-              {item.name}
-            </Link>
-          ))}
-        </div>
+       
 
         
       </div>
